0cd3fb588821cfe66457f6ed1e92fbe68398d6b5
Tabatha Bell <tabathambell@live.com>
2021-03-10 13:48:45 -0500

    fixed row issues with bootstrap

--- src/components/About.js
+++ src/components/About.js
@@ -5,18 +5,18 @@ const About = () => {
     return (
         <div className="container py-5">
             <div className="row">
-                <div className=".col-lg-6 col-xm-12">
+                <div className="col-lg-6 col-xm-12">
                     <div className="photo-wrap mb-5">
 
                         <img className="profile-img" src={image} alt="image of tabatha" />
 
                     </div>
                 </div>
-                <div className=".col-lg-6 col-xm-12">
+                <div className="col-lg-6 col-xm-12">
                     <h1 className="about-heading">About Me</h1>
                     <p className="details">
                         Hello! My name is Tabatha Bell and I am a Florida-based full-stack web developer.
-                        I have an Associate's Degree in English from Ozarks Technical Community College, 
+                        I have an Associate's Degree in English from Ozarks Technical Community College,
                         a Bachelor's Degree in Business Administration from Missouri State University,
                         and a Certification in Web Development through University of Central Florida.
                         I have been programming and coding front-end content for several years and honed my skills
@@ -25,7 +25,7 @@ const About = () => {
                         My diverse set of skills include HTML5 + CSS + Javascript, all of the way up to CSS frameworks
                         such as Bootstrap and UIKit, Responsive Design, JQuery, Node.js, REST, Express.js, MongoDB,
                         Rational Expressions and MySQL.
-                    </p>
+        </p>
                 </div>
             </div>
         </div>
